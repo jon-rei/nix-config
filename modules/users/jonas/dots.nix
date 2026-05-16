7397598b4fc9d9c29a776517f2b1a96e33af888e
Jonas Reineke <mail@jonrei.de>
2026-05-16 09:20:44 +0200

    Fix dots.nix

--- modules/users/jonas/dots.nix
+++ modules/users/jonas/dots.nix
@@ -3,40 +3,40 @@
   home.stateVersion = "25.11";
 
   # Shell
-  programs.zsh = {
-    enable = true;
-    enableCompletion = false;
-    zplug = {
+  programs = {
+    zsh = {
       enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; }
-        { name = "zsh-users/zsh-syntax-highlighting"; }
-        { name = "zsh-users/zsh-completions"; }
-        { name = "zsh-users/zsh-history-substring-search"; }
-      ];
-    };
-    shellAliases = {
-      ll = "ls -alh";
-      update = "sudo nixos-rebuild switch";
-    };
-  };
+      enableCompletion = false;
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "zsh-users/zsh-autosuggestions"; }
+          { name = "zsh-users/zsh-syntax-highlighting"; }
+          { name = "zsh-users/zsh-completions"; }
+          { name = "zsh-users/zsh-history-substring-search"; }
+        ];
+      };
+      shellAliases = {
+        ll = "ls -alh";
+        update = "sudo nixos-rebuild switch";
+      };
 
-  zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  direnv = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  # Git
-  programs.git = {
-    enable = true;
-    settings.user = {
-      name = "Jonas";
-      email = "mail@jonrei.de";
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
     };
+
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    # Git
+    git = {
+      enable = true;
+      settings.user = {
+        name = "Jonas";
+        email = "mail@jonrei.de";
+      };
   };
 }
