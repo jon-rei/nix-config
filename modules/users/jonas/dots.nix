b5a6e52c937d98aea222b6bd9782aa77cf4add6e
Jonas Reineke <mail@jonrei.de>
2026-08-16 09:15:53 +0200

    fix: update command which can be run on the host

--- modules/users/jonas/dots.nix
+++ modules/users/jonas/dots.nix
@@ -13,11 +13,13 @@
       interactiveShellInit = "set -g fish_greeting";
       shellAliases = {
         grep = "rg";
-        update = ''sudo nixos-rebuild switch --flake "git+https://github.com/jon-rei/nix-config#(hostname)" --refresh'';
         ls = "eza";
         ll = "eza -l";
         la = "eza -la";
         lt = "eza --tree";
+      };
+      functions = {
+        update = ''sudo nixos-rebuild switch --flake "git+https://github.com/jon-rei/nix-config#(hostname)" --refresh'';
       };
       plugins = [
         {
